64eab5a2164ae764da1817dddf85c0761fc7a737
Ilya Nefedov <iller1@gmail.com>
2018-03-28 14:49:42 +0500

    for new brunch

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
-(function () {
+const appScreen = document.getElementsByClassName(`main`)[0];
+const templatesHolder = document.getElementById(`templates`).content.children;
+const appTemplates = [];
+const KeyCode = {
+  'ARROW__LEFT': 37,
+  'ARROW__RIGHT': 39
+};
+const firstScreenIndex = 0;
+const lastScreenIndex = templatesHolder.length - 1;
+let currentScreen = 0;
 
-  const appScreen = document.getElementsByClassName(`main`)[0];
-  const templatesHolder = document.getElementById(`templates`).content.children;
-  const appTemplates = [];
-  let currentScreen = 1;
-
-  
-
-
-  function changeScreeen(event) {
-    console.log(event);
-    if (event.altKey && event.keyCode === 37) {
-      showScreen(currentScreen--);
-    } else if (event.altKey && event.keyCode === 39) {
-      showScreen(currentScreen++);
-    }
-    console.log(currentScreen);
+const changeScreen = (e) => {
+  if (e.altKey && e.keyCode === KeyCode.ARROW__LEFT) {
+    currentScreen--;
+    showScreen(currentScreen);
+  } else if (e.altKey && e.keyCode === KeyCode.ARROW__RIGHT) {
+    currentScreen++;
+    showScreen(currentScreen);
   }
+};
 
-  document.addEventListener(`onkeydown`, changeScreeen);
-
-  [].forEach.call(templatesHolder, function (element) {
-    appTemplates.push(element);
-  });
-
-  function showScreen(num) {
-    if (num > appTemplates.length) {
-      currentScreen = 0;
-      appScreen.appendChild(appTemplates[currentScreen]);
-    } else if (num < 0) {
-      currentScreen = appTemplates.length;
-      appScreen.appendChild(appTemplates[currentScreen]);
-    } else {
-      appScreen.appendChild(appTemplates[num]);
-    }
+const showScreen = (num) => {
+  appScreen.innerHTML = ``;
+  if (num > appTemplates.length) {
+    currentScreen = firstScreenIndex;
+    appScreen.appendChild(appTemplates[currentScreen]);
+  } else if (num < 0) {
+    currentScreen = lastScreenIndex;
+    appScreen.appendChild(appTemplates[currentScreen]);
+  } else {
+    appScreen.appendChild(appTemplates[num]);
   }
+};
 
-  showScreen(currentScreen);
+document.addEventListener(`keydown`, changeScreen);
 
-})();
+[].forEach.call(templatesHolder, function (element) {
+  appTemplates.push(element);
+});
+
+showScreen(currentScreen);
